57633c5fe94674c7ebca303c6d0431172cec61fe
magicrobots <adam@magicrobots.com>
2025-12-26 13:21:14 -0500

    Redefine button mappings for U_BTN1, U_BTN2, U_BTN3

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#undef U_BTN1
+#undef U_BTN2
+#undef U_BTN3
+#define U_BTN1 &mkp MB2
+#define U_BTN2 &mkp MB3
+#define U_BTN3 &mkp MB1
